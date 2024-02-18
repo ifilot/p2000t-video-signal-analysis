21a9d5388e09afb6e3e80a311a1993c5691005ba
ifilot <ivo@ivofilot.nl>
2024-02-18 16:05:43 +0100

    Reformatting data

--- analyse_signal.py
+++ analyse_signal.py
@@ -5,37 +5,34 @@ import matplotlib.pyplot as plt
 import scipy
 
 def main():
-    
-    data = np.load('p2000t_video.npy')
-    print(data.shape)
+    time = np.load('time.npy')
+    ch1 = np.load('ch1.npy')
+    ch2 = np.load('ch2.npy')
     
     # create pixel clock
     #x = np.linspace(-2e-5, 5e-5, 100000)
-    pixclk = scipy.signal.square(data[:,0] * 2.0 * np.pi * 6e6) * 2 + 2
-    vsync = scipy.signal.square(data[:,0] * 2.0 * np.pi * 50) * 2 + 2
-    hsync = scipy.signal.square((data[:,0] + 1.45e-5) * 2.0 * np.pi * 15625, 0.93) * 2 + 2
+    pixclk = scipy.signal.square(time * 2.0 * np.pi * 6e6) * 2 + 2
+    vsync = scipy.signal.square(time * 2.0 * np.pi * 50) * 2 + 2
+    hsync = scipy.signal.square((time + 1.45e-5) * 2.0 * np.pi * 15625, 0.93) * 2 + 2
     
     fig, ax = plt.subplots(3,1,dpi=144, figsize=(12,4))
     
-    ax[0].plot(data[:,0], data[:,1], linewidth=0.5)
-    ax[0].plot(data[:,0], data[:,2], linewidth=0.5, alpha=0.5)
-    ax[0].plot(data[:,0], hsync, linewidth=0.5, alpha=0.5)
+    ax[0].plot(time, ch1, linewidth=0.5)
+    ax[0].plot(time, ch2, linewidth=0.5, alpha=0.5)
+    ax[0].plot(time, hsync, linewidth=0.5, alpha=0.5)
     ax[0].set_xlim(-0.001, 0.001)
     ax[0].set_ylim(0,5)
     
-    ax[1].plot(data[:,0], data[:,1], linewidth=0.5)
-    ax[1].plot(data[:,0], data[:,2], linewidth=0.5)
-    ax[1].fill_between(data[:,0], 0, hsync, linewidth=0.5, alpha=0.2)
+    ax[1].plot(time, ch1, linewidth=0.5)
+    ax[1].plot(time, ch2, linewidth=0.5)
+    ax[1].fill_between(time, 0, hsync, linewidth=0.5, alpha=0.2)
     ax[1].set_xlim(-2e-5, 5e-5)
     ax[1].set_ylim(0,5)
     
-    x1=1775000
-    x2=1793000
-    ax[2].plot(data[x1:x2,0], data[x1:x2,1], linewidth=0.5, alpha=0.5)
-    ax[2].plot(data[x1:x2,0], data[x1:x2,2], linewidth=0.5, alpha=0.5)
-
-    ax[2].plot(data[:,0], pixclk, linewidth=0.5)
-    ax[2].plot(data[:,0], (pixclk * data[:,2] > 5) * 4, linewidth=0.5)
+    ax[2].plot(time, ch1, linewidth=0.5, alpha=0.5)
+    ax[2].plot(time, ch2, linewidth=0.5, alpha=0.5)
+    ax[2].plot(time, pixclk, linewidth=0.5)
+    ax[2].plot(time, (pixclk * ch2 > 5) * 4, linewidth=0.5)
     ax[2].set_xlim(0, 2e-5)
     ax[2].set_ylim(0,5)
 
